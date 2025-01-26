2d88cae2b05291b6a901ee5337d4289a4cf8aa5e
David Lozano <chavid54@gmail.com>
2025-01-26 23:26:33 +0100

    Movilidad sin completar

---
+++ Jam2025/Assets/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+    CharacterController controller;
+    [SerializeField] private float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            Vector3 move = Input.GetAxis("Horizontal") * Vector3.right;
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
+        if (Input.GetButton("Vertical"))
+        {
+            Vector3 move = Input.GetAxis("Vertical") * Vector3.forward;
+            controller.Move(move * speed * Time.deltaTime);
+        }
+    }
+}
