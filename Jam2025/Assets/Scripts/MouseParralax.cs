cf2bac2cd81bb48ec0a8e7ec9cb688d1bb2a544b
Cristina <c.martinezo.2021@alumnos.urjc.es>
2025-01-28 20:22:25 +0100

    InterfaceMainMenu

---
+++ Jam2025/Assets/Scripts/MouseParralax.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseParralax : MonoBehaviour
+{
+    [SerializeField] private GameObject[] ParallaxElements;
+    [SerializeField] private float MouseSpeedX = 1f;
+    [SerializeField] private float MouseSpeedY = 0.2f;
+    private Vector3[] OriginalPositions;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined; // cursor can't leave the area
+
+        OriginalPositions = new Vector3[ParallaxElements.Length]; //initialize array lenght
+
+        for ( int i = 0; i< ParallaxElements.Length;i++)
+        {
+            OriginalPositions[i] = ParallaxElements[i].transform.position; // Set original position of each element
+            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float mousex, mousey;
+
+        // Better result across different screen sizes
+        mousex = (Input.mousePosition.x - (Screen.width/2)) * MouseSpeedX/Screen.width; 
+        mousey = (Input.mousePosition.y - (Screen.width/2)) * MouseSpeedY/Screen.width;
+
+        for ( int i = 1;i< ParallaxElements.Length +1; i++)
+        {
+            ParallaxElements[i-1].transform.position = OriginalPositions[i-1] + (new Vector3(mousex, mousey,0f)); 
+            // set position with corresponding offset
+        }
+
+    }
+
+
+}
