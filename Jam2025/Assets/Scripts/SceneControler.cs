3c09cb42cc81246462a8593e8955633d1507522f
Cristina <c.martinezo.2021@alumnos.urjc.es>
2025-01-28 23:57:51 +0100

    PauseMenu

--- Jam2025/Assets/Scripts/SceneControler.cs
+++ Jam2025/Assets/Scripts/SceneControler.cs
@@ -6,8 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-   public void loadGame()
+
+    [SerializeField] private GameObject pauseMenu; 
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+        }
+    }
+
+    public void loadGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void resume()
+    {
+        Time.timeScale = 1.0f;
     }
 }
