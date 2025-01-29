febd261253744ce4382fc5bd5b2fbc426fb33400
Cristina <c.martinezo.2021@alumnos.urjc.es>
2025-01-29 16:58:01 +0100

    ColorCodeWorking

---
+++ Jam2025/Assets/Scripts/ColorCode.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorCode : MonoBehaviour
+{
+    [SerializeField] private Sprite[] colors;
+    [SerializeField] private Image[] display;
+    private string code;
+    private string answer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        code = "";
+        answer = "123";
+
+        for (int i = 0; i < display.Length; i++)
+        {
+            display[i].sprite = colors[0]; // display no color
+        }
+
+        PushButton.ButtonPressed += AddNumberToDisplay; // Subscribe to the event
+    }
+
+    private void AddNumberToDisplay(string value)
+    {
+        if (code.Length < 2)
+        {
+            code += value;
+            display[code.Length-1].sprite = colors[int.Parse(value)];
+        }
+        else
+        {
+            code += value;
+            display[code.Length - 1].sprite = colors[int.Parse(value)];
+            if (code == answer)
+            {
+                Debug.Log("Correct");
+                Invoke("Reset", 0.5f);
+            }
+            else
+            {
+                Debug.Log("Wrong");
+                Invoke("Reset", 0.5f);
+            }
+        }
+    }
+
+
+    private void Reset()
+    {
+        code = "";
+
+        for (int i = 0; i < display.Length; i++)
+        {
+            display[i].sprite = colors[0];
+        }
+
+    }
+
+    private void OnDestroy()
+    {
+        PushButton.ButtonPressed -= AddNumberToDisplay;
+    }
+}
+
