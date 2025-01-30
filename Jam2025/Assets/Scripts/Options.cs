a32c0e0e9fdcc099d9331269b57b466894d0ce4a
Cristina <c.martinezo.2021@alumnos.urjc.es>
2025-01-30 13:54:50 +0100

    OptionsWorking

---
+++ Jam2025/Assets/Scripts/Options.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+
+    [SerializeField] private Slider ambientSound; 
+    [SerializeField] private AudioMixer ambientMixer; 
+    [SerializeField] private Slider soundEffect;
+    [SerializeField] private AudioMixer effectMixer;
+    [SerializeField] private Slider sensibility;
+
+    void Start()
+    {
+        ambientSound.value = PlayerPrefs.GetFloat("VolumeAmbient");
+        soundEffect.value = PlayerPrefs.GetFloat("VolumeEffect");
+        sensibility.value = PlayerPrefs.GetFloat("Sensibility");
+    }
+
+    public void SetAmbientVolume(float volume)
+    {
+        ambientMixer.SetFloat("VolumeAmbient", volume);
+        PlayerPrefs.SetFloat("VolumeAmbient", volume);
+    }
+
+    public void SetEffectVolume(float volume)
+    {
+        effectMixer.SetFloat("VolumeEffect", volume);
+        PlayerPrefs.SetFloat("VolumeEffect", volume);
+    }
+
+    public void SetSensibility(float value)
+    {
+        PlayerPrefs.SetFloat("Sensibility", value);
+    }
+
+
+
+}
